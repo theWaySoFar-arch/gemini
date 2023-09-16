05a196f32a379b53e58e4cf493dbe95b08347cae
fanxingmeng <fanxing_meng@hotmail.com>
2023-09-16 14:26:33 +0800

    查询条件确定

---
+++ gemini-server/src/main/java/org/apache/server/dto/LogSearchParamsDTO.java
@@ -0,0 +1,25 @@
+package org.apache.server.dto;
+
+import java.time.LocalDateTime;
+
+public class LogSearchParamsDTO {
+
+    public LocalDateTime startTime;
+
+    public LocalDateTime endTime;
+
+    public String appName;
+
+    public String enviorment;
+
+    public String level;
+
+    public String className;
+
+    public String traceId;
+
+    public String context;
+
+    public String serverName;
+
+}
